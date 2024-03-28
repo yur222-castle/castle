aa1cbf558d715ac3d68f91c32afc3fb5f0890b9b
yur222-castle <156274945+yur222-castle@users.noreply.github.com>
2024-03-28 11:53:14 +0900

    Add files via upload

--- F-1/js/script.js
+++ F-1/js/script.js
@@ -8,37 +8,30 @@ $(function () {
         $(this).find('.sub').stop().slideUp();
     });
 
-
-
-    $('.open').click(function () {
-        $('.popup').show();
-
-    });
-
-    $('button').click(function () {
-        $('.popup').hide();
-
-    });
-
-
     let t = 0;
-    $('.tabmenu>p').click(function () {
-        $('.tabmenu>p').removeClass('on');
-        $(this).addClass('on')
+    $('.tab_menu>p').click(function () {
+        $('.tab_menu>p').removeClass('on');
+        $(this).addClass('on');
 
         t = $(this).index();
         $('.con').hide();
         $('.con').eq(t).show();
-    });
-
+    })
 
 
     let c = 0;
     setInterval(function () {
         let n = (c + 1) % 3;
-        $('.slider').eq(c).fadeOut(1200);
-        $('.slider').eq(n).fadeIn(1200);
+        $('.slider').eq(c).fadeOut(1000);
+        $('.slider').eq(n).fadeIn(1000);
         c = n;
-    }, 3000);
+    }, 4000);
+
+    $('.open').click(function () {
+        $('.popup').show();
+    });
+    $('button').click(function () {
+        $('.popup').hide();
+    });
 
 });
